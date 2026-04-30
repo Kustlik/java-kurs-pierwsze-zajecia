88001b984e0c9e79d07d620226ff7f1c75e96637
Łukasz Sobala <77438823+Kustlik@users.noreply.github.com>
2026-04-30 10:04:41 +0200

    Update HelloWorld.java

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -10,7 +10,7 @@ public class HelloWorld {
 // Sprawdzenie czy to JDK 17+
         int major = Integer.parseInt(javaVersion.split("\\.")[0]);
         if (major >= 17) {
-            System.out.println("OK: Używamy Java " + major + " — wszystkie funkcje dostepne" + "funkconalnosc A");
+            System.out.println("OK: Używamy Java " + major + " — wszystkie funkcje dostepne" + "funkconalnosc A" + "conflict");
         } else {
             System.out.println("UWAGA: Za stara wersja Java. Wymagana: 17+");
         }
